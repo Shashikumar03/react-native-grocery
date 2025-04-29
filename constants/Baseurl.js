b1bfff6ae5b148e9e94080f3b66a96131ddd89cf
ShashiCard91 <shashi.kushwaha@card91.io>
2025-04-29 16:39:04 +0530

    grocery app

--- constants/Baseurl.js
+++ constants/Baseurl.js
@@ -1,5 +1,5 @@
 export function getBaseUrl() {
-    return "http://192.168.0.155:9191"
+    return "http://192.168.65.142:8081"
     // "anty wifi"
       // return "http://192.168.0.189:8283";
       // "vishal"
